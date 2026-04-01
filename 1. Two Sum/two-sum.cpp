20e8f9d073862da4febc1b63eff4268ff68d209e
tarcantcode <wiletgg@gmail.com>
2026-04-02 01:59:10 +0700

    สามเหลี่ยมเบอร์มิวด้า bermuda triangle shape code O(n2)
    latency ช้าแต่โค๊ดสวยชิบหาย

--- 1. Two Sum/two-sum.cpp
+++ 1. Two Sum/two-sum.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
             for (int i = 0; i < nums.size(); i++) {
-                for (int j = i + 1; j < nums.size(); j++) {
-                    if (nums[i] + nums[j] == target){
+                for (int j = i + 1; j < nums.size(); j++) {  // เห็นสามเหลี่ยมไหมละพวกนาย โครตสวยเลย O(n2) 📐 
+                    if (nums[i] + nums[j] == target){ // that we call an art! 📐 
                         return {i, j};
                     }
                 }
             }
-            return {};
+            return {}; // running slow, but the shape of the code look good, so i take it.
     }
 };
