b6986cd24a69d7b6ddf1b653b13fb820de062b31
tarcantcode <wiletgg@gmail.com>
2026-04-06 23:32:22 +0700

    Implement mergeTwoLists function to merge two sorted lists

---
+++ 21. Merge Two Sorted Lists/merge-two.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* cur = dummy;
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val <= list2->val) {
+                cur->next = list1;
+                list1 = list1->next;
+            } else { 
+                cur->next = list2;
+                list2 = list2->next;
+            }
+            cur = cur->next;
+        }
+        cur->next = list1 ? list1 : list2;
+        return dummy->next;
+        
+    }
+};
